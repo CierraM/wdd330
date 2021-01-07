093e6612f8c866c753ae7057c343d4252abf0ec8
Cierra <4cibear@gmail.com>
2021-01-06 19:49:58 -0700

    story writer from week 1

---
+++ js/main.js
@@ -0,0 +1,19 @@
+const links = [
+    {
+    label: "Week 01 Notes",
+    url: "../week01/index.html"
+    }
+];
+
+const container = document.getElementById('links');
+
+links.map( link => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+
+    a.textContent = link.label;
+    a.setAttribute('href', link.url);
+
+    li.appendChild(a);
+    container.appendChild(li);
+})
